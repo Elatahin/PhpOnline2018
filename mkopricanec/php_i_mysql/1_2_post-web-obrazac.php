c9f474093416e2dadf805a629c644d834b0940fd
Elatahin <markoantonijekoprichanec@gmail.com>
2018-11-21 10:11:06 +0100

    Ponavljao

---
+++ mkopricanec/php_i_mysql/1_2_post-web-obrazac.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>
+            
+            <form> 
+                <input type ="text" name="ime" value="">
+                <br>
+                <input type ="text" name="prezime" value="">
+                <br>
+                <input type="submit" name="submit_btn" value="obrada">
+                
+            </form>
+        
+        </div>
+        <div>
+           <?php
+           echo "poz";
+            
+        </div>
+        
+    </body>
+</html>
